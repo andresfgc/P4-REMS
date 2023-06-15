6bd977797cd33725ed4c3236c26fd4a06bd34358
Andresfgc <afgarciacano@gmail.com>
2023-06-15 18:50:55 +0000

    Allowed hosted changed

--- django_rems/settings.py
+++ django_rems/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['rems-ag.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['rems-ag-58c10e6f7952.herokuapp.com', 'localhost']
 
 
 # Application definition
