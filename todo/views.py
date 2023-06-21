113207101e5411c288b3a4d8b1564e7147ece747
Andresfgc <afgarciacano@gmail.com>
2023-06-21 19:41:09 +0000

    Add create property function

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Project, Property
 # Create your views here.
 
@@ -9,3 +9,15 @@ def get_todo_list(request):
         'properties': properties
     }
     return render(request, 'todo/todo_list.html', context)
+
+
+def add_property(request):
+    if request.method == 'POST':
+        property_number = request.POST.get('property_number')
+        Status = request.POST.get('Status')
+        Price = request.POST.get('Price')
+        #  done = 'done' in request.POST
+        Property.objects.create(property_number=property_number, Status=Status, Price=Price)
+
+        return redirect('get_todo_list')
+    return render(request, 'todo/add_property.html')
